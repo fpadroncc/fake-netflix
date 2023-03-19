202c8edcd0a5d85bdfa72afe665556d14c560d00
Francisco Javier Padron Luis <franciscopadron@capitole-consulting.com>
2023-03-19 21:38:35 +0100

    feat: :lipstick: added button toogle for create acc

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,10 +1,18 @@
 import Input from "@/components/input";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
+
+  const [variant, setVariant] = useState("login");
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) =>
+      currentVariant === "login" ? "register" : "login"
+    );
+  }, []);
 
   return (
     <div className="relative h-screen w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -14,7 +22,9 @@ const Auth = () => {
         </nav>
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-            <h2 className="text-white text-4xl mb-8 font-semibold">Sig In</h2>
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {variant === "login" ? "Sign In" : "Register"}
+            </h2>
             <div className="flex flex-col gap-4">
               <Input
                 label="UserName"
@@ -36,6 +46,18 @@ const Auth = () => {
                 value={password}
               />
             </div>
+            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+              Login
+            </button>
+            <p className="text-neutral-500 mt-12">
+              First time using Netflix?
+              <span
+                className="text-white ml-1 hover:underline cursor-pointer"
+                onClick={toggleVariant}
+              >
+                Create and account
+              </span>
+            </p>
           </div>
         </div>
       </div>
